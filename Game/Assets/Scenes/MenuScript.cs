060e1202aefcf7ace19851f0e620403404a9d859
Sankar Renganathan <renganas@mcmaster.ca>
2019-03-26 20:20:00 -0400

    Added home screen

---
+++ Game/Assets/Scenes/MenuScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame(){
+        Application.LoadLevel(1);
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+}
