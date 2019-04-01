923bd5456107bb43c2f2b50710794b2b95c535dd
Sankar Renganathan <renganas@mcmaster.ca>
2019-04-01 19:16:59 -0400

    Added enemy movement

--- Game/Assets/Movement.cs
+++ Game/Assets/Movement.cs
@@ -19,5 +19,23 @@ public class Movement : MonoBehaviour
             position.x--;
             this.transform.position = position;
         }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.y--;
+            this.transform.position = position;
+        }
     }
 }
