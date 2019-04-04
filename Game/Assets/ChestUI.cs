6d90674bb6e036813db5d2aa741e86a7f6d96a51
Sankar Renganathan <renganas@mcmaster.ca>
2019-04-04 18:21:39 -0400

    Added Chest UI

--- Game/Assets/ChestUI.cs
+++ Game/Assets/ChestUI.cs
@@ -2,16 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chest : MonoBehaviour
+public class ChestUI : MonoBehaviour
 {
     private Animator anim;
     private int open = -1;
     private Transform player;
+    private Transform chest;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        chest = GameObject.FindGameObjectWithTag("Chest").transform;
     }
 
     // Update is called once per frame
@@ -19,34 +21,34 @@ public class Chest : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (Vector3.Distance(transform.position, player.position) < 1.5)
+            if (Vector3.Distance(chest.position, player.position) < 1.5)
             {
                 open = open*-1;
                 if (open > 0){
-                    OpenChest();
+                    OpenChestUI();
                 }
                 else if (open < 0){
-                    CloseChest();
+                    CloseChestUI();
                 }
             }
             else
             {
                 open = -1;
-                CloseChest();
+                CloseChestUI();
             }
         }
-        else if (Vector3.Distance(transform.position, player.position) > 1.5)
+        else if (Vector3.Distance(chest.position, player.position) > 1.5)
         {
             open = -1;
-            CloseChest();
-        }
+            CloseChestUI();
+        }  
     }
 
-    private void OpenChest(){
-        anim.SetTrigger("open");
+    private void OpenChestUI(){
+        anim.SetBool("chest", true);
     }
 
-    private void CloseChest(){
-        anim.ResetTrigger("open");
+    private void CloseChestUI(){
+        anim.SetBool("chest", false);
     }
 }
