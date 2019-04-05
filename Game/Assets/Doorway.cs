e6c7d120df389493dad7d72a52a3e547fcca5fe5
Sankar Renganathan <renganas@mcmaster.ca>
2019-04-05 19:43:22 -0400

    Fixed bug when loading rooms

--- Game/Assets/Doorway.cs
+++ Game/Assets/Doorway.cs
@@ -8,11 +8,13 @@ public class Doorway : MonoBehaviour
 {
     private Transform player;
     private GameObject[] enemies;
+    private GameObject wizard;
     // Start is called before the first frame update
     void Start()
     {
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        wizard = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
@@ -23,6 +25,7 @@ public class Doorway : MonoBehaviour
         {
             if (Vector3.Distance(transform.position, player.position) < 1.1)
             {
+                DontDestroyOnLoad(wizard);
                 SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
             }
         }
