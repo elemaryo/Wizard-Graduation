9d6c3ebf0168d53baf920a6eeecfabeaa7132486
Omar Elemary <amora@Omar-Elemarys-MacBook-Pro.local>
2019-04-06 20:33:39 -0400

    Menus/Fixes

--- Game/Assets/Player.cs
+++ Game/Assets/Player.cs
@@ -8,7 +8,12 @@ public class Player : MonoBehaviour
     private bool attack;
 
     public GameObject projectile;
+
+    [SerializeField]
+    public GameObject[] spellPrefab;
+
     private Animator anim;
+
 
     void Start()
     {
@@ -22,41 +27,151 @@ public class Player : MonoBehaviour
         transform.position += objpos * speed * Time.deltaTime;
 
         //Face Hat towards Mouse (pointy side away from mouse)
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 0;
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = 0;
+
+        //Mouse follow
+        FaceMouse();
+
+        //Cast spells method
+        CastSpell();
 
         Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
-        mousePos.x = mousePos.x - objectPos.x;
-        mousePos.y = mousePos.y - objectPos.y;
+        mousePosition.x = mousePosition.x - objectPos.x;
+        mousePosition.y = mousePosition.y - objectPos.y;
 
-        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(mousePosition.y, mousePosition.x) * Mathf.Rad2Deg;
         angle -= 90; //correction for angle, sprite was sideways
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 
-        if (Input.GetKeyDown(KeyCode.Space)) // mouse click Input.GetMouseButtonDown(0)
-        {
-            Instantiate(projectile, transform.position, Quaternion.identity);
-            attack = true;
-            DoAttack();
-            //spriteRenderer.sprite = wizAttack; // attack
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            attack = false;
-            DoIdle();
-            //spriteRenderer.sprite = wizIdle;
-        }
     }
 
-    private void DoAttack(){
-        if (attack){
+    private void FaceMouse()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x = transform.position.x, mousePosition.y = transform.position.y);
+        transform.up = -direction;
+    }
+
+    public void DoAttack()
+    {
+        if (attack)
+        {
             anim.SetTrigger("attack");
         }
     }
 
-    private void DoIdle(){
-        if(!attack){
+    public void DoIdle()
+    {
+        if (!attack)
+        {
             anim.ResetTrigger("attack");
         }
+    }
+
+    public void ClickSpell(int spellIndex)
+    {
+        if (spellIndex == 0)
+        {
+            Instantiate(spellPrefab[0], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+        else if (spellIndex == 1)
+        {
+            Instantiate(spellPrefab[1], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+        else if (spellIndex == 2)
+        {
+            Instantiate(spellPrefab[2], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+        else if (spellIndex == 3)
+        {
+            Instantiate(spellPrefab[3], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+        else if (spellIndex == 4)
+        {
+            Instantiate(spellPrefab[4], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+        else if (spellIndex == 5)
+        {
+            Instantiate(spellPrefab[5], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+    }
+
+    private void CastSpell()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Instantiate(spellPrefab[0], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Instantiate(spellPrefab[1], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(spellPrefab[2], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Instantiate(spellPrefab[3], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Instantiate(spellPrefab[4], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Instantiate(spellPrefab[5], transform.position, Quaternion.identity);
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            attack = true;
+            DoAttack();
+        }
+
+        else
+        {
+            attack = false;
+            DoIdle();
+        }
+
     }
 }
---
+++ Game/Assets/Reticle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reticle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hide mouse cursor
+        Cursor.visible = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Input.mousePosition;
+    }
+}
