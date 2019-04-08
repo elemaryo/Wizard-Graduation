bc3ee18bfa707a853006c4b97bc79c24a475b96f
ZuhairMakda <46846803+ZuhairMakda@users.noreply.github.com>
2019-04-08 13:20:22 -0400

    Added Health and Projectile Attach

---
+++ Game/Assets/Player Scripts/PlayerHealth.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    private GameObject health;
+
+    Player player;
+    //bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Player>();
+        currentHealth = startingHealth;
+        health = GameObject.FindGameObjectWithTag("Healthslider");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthSlider.value = currentHealth;
+    }
+
+    public void takeDamage (int amount)
+    {
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    private void Death()
+    {
+        //restart game?
+        //Add death cutscene here
+        SceneManager.LoadScene("Home");
+        Destroy(gameObject);
+        Destroy(health);
+
+        //playerMovement.enabled = false;
+
+    }
+
+
+}
