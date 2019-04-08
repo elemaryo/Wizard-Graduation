fcb0f1b6fef4e95d10eab28a5c09a5b0c7af9957
Sankar Renganathan <renganas@mcmaster.ca>
2019-04-07 21:54:24 -0400

    FINALLY IMPLEMENTED A FULL CHEST SYSTEMgit add *git add *

--- Game/Assets/Game Scripts/ChestSlot.cs
+++ Game/Assets/Game Scripts/ChestSlot.cs
@@ -6,17 +6,28 @@ using UnityEngine.UI;
 public class ChestSlot : MonoBehaviour
 {
     public Image icon;
-    private int item;
+    private GameObject item;
 
-    public void AddItem ()
+    void Start()
     {
-        Debug.Log("Add Item");
-        icon.sprite = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void AddItem(GameObject newItem)
+    {
+        item = newItem;
+        //icon.sprite = null;
+        icon.sprite = newItem.GetComponent<SpriteRenderer>().sprite;
         icon.enabled = true;
     }
 
     public void ClearSlot()
     {
+        item = null;
         icon.sprite = null;
         icon.enabled = false;
     }
