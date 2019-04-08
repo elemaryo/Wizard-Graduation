da8453c0883029d977ebdaf0201bd16628c969be
Sankar Renganathan <renganas@mcmaster.ca>
2019-04-08 16:06:52 -0400

    Small changes

---
+++ Game/Assets/Game Scripts/InventorySlot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    private GameObject item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        item = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddItem(GameObject newItem)
+    {
+        item = newItem;
+        //icon.sprite = null;
+        icon.sprite = newItem.GetComponent<SpriteRenderer>().sprite;
+        icon.enabled = true;
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+        icon.sprite = null;
+        icon.enabled = false;
+    }
+}
